a0f37917ef093a806a393fa03ae1ed5df6c1a876
Guilhermo Masid <Guilhermo@MacBook-Air-de-Guilhermo.local>
2020-05-05 17:45:34 -0300

    mudando estilo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -31,7 +31,7 @@ export default function Header() {
                   profile.avatar.url ||
                   'https://skywarehouse.s3.amazonaws.com/staticfiles/warehouse/img/nouser.png'
                 }
-                alt="Ver perfil"
+                alt={profile.name}
               />
             </Link>
           </Profile>
